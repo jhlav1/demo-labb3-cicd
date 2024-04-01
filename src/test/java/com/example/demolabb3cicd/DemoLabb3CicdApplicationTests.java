50ba479c8684d3417ca8578df250da5443792871
jhlav <jher02@hotmail.com>
2024-04-01 22:29:32 +0200

    added checkWebsiteTitle

--- src/test/java/com/example/demolabb3cicd/DemoLabb3CicdApplicationTests.java
+++ src/test/java/com/example/demolabb3cicd/DemoLabb3CicdApplicationTests.java
@@ -1,7 +1,12 @@
 package com.example.demolabb3cicd;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -14,13 +19,36 @@ class DemoLabb3CicdApplicationTests {
 
 	WebDriver driver;
 
-	@Test
-	void checkWebsiteTitle() {
+	@BeforeEach
+	void setup(){
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--headless");
 		driver = new ChromeDriver(options);
+	}
+
+	@Test
+	void checkWebsiteTitle() {
+		/*ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless");
+		driver = new ChromeDriver(options); */
 		driver.get("https://www.iths.se");
 		assertEquals("IT-Högskolan – Här startar din IT-karriär!", driver.getTitle(), "The title does not match");
 	}
+	@Test
+	void checkHeading(){
+
+		driver.get("https://www.iths.se");
+		WebElement heading = driver.findElement(By.tagName("h1"));
+		String headingText = heading.getText();
+		Assertions.assertEquals("Här startar din IT-karriär!", headingText);
+
+	}
+
+	@AfterEach
+	void teardown(){
+		driver.quit();
+	}
+
+
 
 }
